7d20cdc8f24250cec14a00ea2648d508220e4eac
Heytemisan <temyicy@gmail.com>
2022-07-25 14:21:48 +0100

    first Rest API status

--- backend/server.js
+++ backend/server.js
@@ -5,4 +5,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({message: 'Success!'});
+}) 
+
 app.listen(port, () => console.log(`Server started on ${port}`));
