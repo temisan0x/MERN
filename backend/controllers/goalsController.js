06f96ccdaa46660d679d5dbbbe2f156f71fa4a30
Heytemisan <temyicy@gmail.com>
2022-07-25 15:00:36 +0100

    chore: added comments

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,14 +1,30 @@
+//@desc getGoals
+//@route GET POST /api/goals
+//@access PRIVATE
+
 const getGoals = (req, res) => {
     res.status(200).json({ message: 'Get Goals!' });
 };
+
+//@desc postGoals
+//@route POST /api/goals
+//@access PRIVATE
 
 const postGoals = (req, res) => {
     res.status(200).json({ message: 'Set Goals!' });
 };
 
+//@desc updateGoals
+//@route  update /api/goals
+//@access PRIVATE
+
 const updateGoal = (req, res) => {
     res.status(200).json({ message: `UpdateGoal! ${req.params.id}` });
 }
+
+//@desc deleteGoals
+//@route DELETE POST /api/goals
+//@access PRIVATE
 
 const deleteGoal = (req, res) => {
     res.status(200).json({ message: `DeleteGoal! ${req.params.id}` });
