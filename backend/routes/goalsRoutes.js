27b065569e3413bd9d826f99de79ee8142c225b5
Heytemisan <temyicy@gmail.com>
2022-07-25 14:38:48 +0100

    feat: route setup

--- backend/routes/goalsRoutes.js
+++ backend/routes/goalsRoutes.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({message: 'Success!'});
-}) 
+    res.status(200).json({ message: 'Get Goals!' });
+});
+
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'Set Goals!' });
+});
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ message: `UpdateGoal! ${req.params.id}`  });
+});
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ message: `DeleteGoal! ${req.params.id}` });
+});
 
 module.exports = router
