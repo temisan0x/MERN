749d3a484bdcba085b07cf033f1a0bb8e86d808f
Heytemisan <temyicy@gmail.com>
2022-07-25 16:05:38 +0100

    feat: added errorHandler

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { listeners } = require('process');
 const dotenv = require('dotenv').config();
+const { errroHandler, errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 //middleware
-app.use(express.json());
+app.use(express.json());     
+app.use(errorHandler)
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalsRoutes'));
