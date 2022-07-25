d7fecb40abf1f449043727cf536d56fc118e3b1e
Heytemisan <temyicy@gmail.com>
2022-07-25 14:32:51 +0100

    get request - goalsRoutes

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Success!'});
-}) 
+app.use('/api/goals', require('./routes/goalsRoutes')) 
 
 app.listen(port, () => console.log(`Server started on ${port}`));
