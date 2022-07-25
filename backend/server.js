55f0f58e7ace2550412bf320bb68150a671ffb16
Heytemisan <temyicy@gmail.com>
2022-07-25 22:15:40 +0100

    feat: code cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const { listeners } = require('process');
 const dotenv = require('dotenv').config();
-const { errroHandler, errorHandler } = require('./middleware/errorMiddleware');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 //middleware
-app.use(express.json());     
-app.use(errorHandler)
+app.use(express.json());    
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalsRoutes'));
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on ${port}`));
