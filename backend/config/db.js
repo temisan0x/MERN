a0e080028bf10d6700539ca019d9ba1cebb8774b
Heytemisan <temyicy@gmail.com>
2022-07-28 18:34:26 +0100

    chore: made adjustments to mongo db compass

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Connected to mongoose ${conn.connection.host}`.green.underline);
+        console.log(`Connected to mongoose ${conn.connection.host}`.bgMagenta.underline);
     } catch (error) {
         console.log(error);
         process.exit(1);
