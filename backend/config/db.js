da12b7ace576931b81d8bc8f685446711655f19e
Heytemisan <temyicy@gmail.com>
2022-07-28 14:17:47 +0100

    chore: colors

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,9 @@
-const { cyan } = require('colors');
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Connected to mongoose ${conn.connection.host}`, cyan.underline);
+        console.log(`Connected to mongoose ${conn.connection.host}`.green.underline);
     } catch (error) {
         console.log(error);
         process.exit(1);
