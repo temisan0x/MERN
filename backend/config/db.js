9266ecfcf6b82182e994c24b04799e113f2d6a2e
Heytemisan <temyicy@gmail.com>
2022-07-29 22:12:36 +0100

    chore: code cleanup

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Connected to mongoose ${conn.connection.host}`.bgMagenta.underline);
+        console.log(`Connected to mongoDB ${conn.connection.host}`.bgMagenta.underline);
     } catch (error) {
         console.log(error);
-        process.exit(1);
+        process.exit(1);//exist process with failure
     }
 }
 
-module.exports = connectDB;
+module.exports = connectDB
