2d3dbf18a83d687fb1b567bc34242615433129ab
Heytemisan <temyicy@gmail.com>
2022-07-29 23:23:57 +0100

    feat: User routes

--- backend/models/notesModel.js
+++ backend/models/notesModel.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 //field for data field resource
 
 const notesSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     text: {
         type: String,
         required: [true, "Please add a title value"]
