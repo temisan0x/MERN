bc057e80dd529ce77ca4c3cf6cc86dd6f7c2c160
Heytemisan <temyicy@gmail.com>
2022-07-29 23:15:16 +0100

    feat: email, password => userSchema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,5 +4,14 @@ const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add a email address'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
     }
 })
