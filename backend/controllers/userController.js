0ea9a02124a1147c055afd8744365c3f08fa32ad
Heytemisan <temyicy@gmail.com>
2022-07-30 23:19:48 +0100

    feat: generate user token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,6 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(500)
@@ -72,5 +73,11 @@ const getUser = asyncHandler(async (req, res) => {
     res.json({ message: 'user data displayed' });
 });
 
+//Generate a JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn:'30d',
+    })
+}
 
 module.exports = { registerUser, loginUser, getUser }
