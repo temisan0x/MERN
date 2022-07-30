6765ba40865d24c35020906d3f54f660bcc65b00
Heytemisan <temyicy@gmail.com>
2022-07-30 23:06:53 +0100

    chore: code cleanup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,7 +52,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-        res.join({
+        res.json({
             _id: user.id,
             name: user.name,
             email: user.email,
