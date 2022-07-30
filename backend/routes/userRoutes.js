fa3d5b955617da79314662eba7d92f761799c76c
Heytemisan <temyicy@gmail.com>
2022-07-30 22:05:06 +0100

    feat: created user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { registerUser } = require('../controllers/userController');
+const { registerUser, loginUser, getUser } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/', registerUser);
+router.post('/login', loginUser);
+router.get('/me', getUser)
 
 module.exports = router;
