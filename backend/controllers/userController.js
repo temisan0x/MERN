60ab4160d64165bdd56aa9818978c4eea264e0e5
Heytemisan <temyicy@gmail.com>
2022-07-30 22:22:42 +0100

    feat: userController register validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,27 +1,37 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
 // @desc Register new user
 //@route POST /api/users
 //@access Public
 
-const registerUser = (req, res) => {
-    res.json({message: 'Registration successful.'});
-}
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please provide all fields');
+    }
+    res.json({ message: 'Registration user.' });
+});
 
 // @desc Authenticate user
 //@route POST /api/users/login
 //@access Public
 
-const loginUser = (req, res) => {
-    res.json({message: 'Login user'});
-}
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({ message: 'Login user' });
+});
 
 
 // @desc Get user data
 //@route POST /api/users/me
 //@access Public
 
-const getUser = (req, res) => {
-    res.json({message: 'user data displayed'});
-}
+const getUser = asyncHandler(async (req, res) => {
+    res.json({ message: 'user data displayed' });
+});
 
 
 module.exports = {registerUser, loginUser, getUser}
