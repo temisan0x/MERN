c8016a86f5cbb9771d9331155fee35e9bcdd5aa0
Heytemisan <temyicy@gmail.com>
2022-07-30 22:02:34 +0100

    feat: userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,27 @@
+// @desc Register new user
+//@route POST /api/users
+//@access Public
+
 const registerUser = (req, res) => {
     res.json({message: 'Registration successful.'});
 }
 
-module.exports = registerUser
+// @desc Authenticate user
+//@route POST /api/users/login
+//@access Public
+
+const loginUser = (req, res) => {
+    res.json({message: 'Login user'});
+}
+
+
+// @desc Get user data
+//@route POST /api/users/me
+//@access Public
+
+const getUser = (req, res) => {
+    res.json({message: 'Registration successful.'});
+}
+
+
+module.exports = {registerUser, loginUser, getUser}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const registerUser = require('../controllers/userController');
+const { registerUser } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/', registerUser);
