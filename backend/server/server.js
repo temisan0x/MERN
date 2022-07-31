4eafccec2cfadbd0e1148638c80cc41303216f1c
Heytemisan <temyicy@gmail.com>
2022-07-31 12:19:15 +0100

    feat: created a server folder

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
+const { errorHandler } = require('../middleware/errorMiddleware');
 const colors = require('colors')
 const port = process.env.PORT || 5000; 
-const connectDB = require('./config/db')
+const connectDB = require('../config/db')
 
 
 connectDB();
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //routes
 app.use(
-    '/api/notes', require('./routes/noteRoutes')
+    '/api/notes', require('../routes/noteRoutes')
 );
 app.use(
-    '/api/users', require('./routes/userRoutes')
+    '/api/users', require('../routes/userRoutes')
 )
 //end-point error handler
 app.use(errorHandler)
