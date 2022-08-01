072b1392fbfdc1b8c7819e06af7ea0db2b6d0c14
Heytemisan <temyicy@gmail.com>
2022-08-01 05:46:10 +0100

    feat: protected note Routes

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {getNotes, setNotes,updateNote,deleteNote} = require('../controllers/notesController');
 
+const { protect } = require("../middleware/authMiddleware");
 
-router.route('/').get(getNotes).post(setNotes);
-router.route('/:id').put(updateNote).delete(deleteNote);
+router.route('/').get(protect, getNotes).post(protect, setNotes);
+router.route('/:id').put(protect, updateNote).delete(protect, deleteNote);
 
 module.exports = router; 
