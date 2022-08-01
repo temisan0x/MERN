d29bd8fc6cb263724e11f862e41537076c1cf04f
Heytemisan <temyicy@gmail.com>
2022-08-01 16:52:44 +0100

    feat: user && note validation

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Note = require('../models/notesModel');
+const User = require('../models/userModel');
 //mongoose returns a promise, we will there require an asyncHandler  by default
 //@desc getNotes
 //@route GET POST /api/notes
@@ -38,14 +39,29 @@ const updateNote = asyncHandler(async (req, res) => {
     if (!note) {
         res.status(400);
         throw new Error('Note not found');
+    };
+    
+    const user = await User.findById(req.user.id);
+
+    //check for user
+    if (!user) { 
+        res.status(401);
+        throw new Error('User not found');
+    };
+
+    //make user the logged in user matches the goal user
+    if (note.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('Note does not match user');
     }
+
     //Submitted a payload of data && an id
     const updatedNote = await Note.findByIdAndUpdate(
         req.params.id,
         req.body, {
             new: true
         }
-    )
+    ) 
     res.status(200).json(updatedNote);
 })
 
@@ -59,6 +75,21 @@ const deleteNote = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Note not found');
     }
+
+    const user = await User.findById(req.user.id);
+
+    //check for user
+    if (!user) { 
+        res.status(401);
+        throw new Error('User not found');
+    };
+
+    //make user the logged in user matches the goal user
+    if (note.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('Note does not match user');
+    }
+
 
     await Note.remove()
     res.status(200).json({id: req.params.id});
