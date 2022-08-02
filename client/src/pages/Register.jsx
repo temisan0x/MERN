ce210bdfb78ca3b490f6437e23b894dd217c9b9a
Heytemisan <temyicy@gmail.com>
2022-08-02 07:06:24 +0100

    feat: register form section

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react"
+
 function Register() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+    });
+
+    const { name, email, password, password2 } = formData;
+
     return (
-        <div>Register</div>
+        <>
+            <section className="heading">
+                <h1>Register</h1>
+                <p>Please create an account</p>
+            </section>
+            <section className="form">
+                <form>
+                    <div className="form-group">
+                        <input type="text" value={name} placeholder="Enter your name dear friend" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" value={name} placeholder="Enter your name dear friend" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" value={name} placeholder="Enter your name dear friend" className="form-control" />
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
 
