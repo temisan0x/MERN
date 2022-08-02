669ae3f9149c7df84ff57df9333d68a2c76a7db8
Heytemisan <temyicy@gmail.com>
2022-08-02 07:53:31 +0100

    feat: react-icons

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -11,22 +11,60 @@ function Register() {
 
     const { name, email, password, password2 } = formData;
 
+    function onChange(e) {
+        return setFormData((prevValue) => ({
+            ...prevValue,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <>
             <section className="heading">
                 <h1>Register</h1>
                 <p>Please create an account</p>
             </section>
-            <section className="form">
+            <section className="form" onSubmit={onSubmit}>
                 <form>
                     <div className="form-group">
-                        <input type="text" value={name} placeholder="Enter your name dear friend" className="form-control" />
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            value={name}
+                            placeholder="Enter your name" className="form-control"
+                            onChange={onChange} />
                     </div>
                     <div className="form-group">
-                        <input type="text" value={name} placeholder="Enter your name dear friend" className="form-control" />
+                        <input type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            placeholder="Enter your email" className="form-control"
+                            onChange={onChange} />
                     </div>
                     <div className="form-group">
-                        <input type="text" value={name} placeholder="Enter your name dear friend" className="form-control" />
+                        <input type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Enter your password" className="form-control"
+                            onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input type="password"
+                            id="password2"
+                            name="password2"
+                            value={password2}
+                            placeholder="Confirm password" className="form-control"
+                            onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-block" type="submit">Submit</button>
                     </div>
                 </form>
             </section>
