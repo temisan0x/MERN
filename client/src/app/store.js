d5be6e4d5da088928220e95836f8d907af86dec5
Heytemisan <temyicy@gmail.com>
2022-08-04 11:28:11 +0100

    feat: authSlice

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../redux/authSlice'
 
 export const store = configureStore({
   reducer: {
-
+    auth: authReducer
   },
 });
