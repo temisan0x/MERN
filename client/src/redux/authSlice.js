c92b534600a9805e9d4bb65d00028cdecfb3f674
Heytemisan <temyicy@gmail.com>
2022-08-05 16:55:25 +0100

    chore: code cleanup

--- client/src/redux/authSlice.js
+++ client/src/redux/authSlice.js
@@ -13,15 +13,16 @@ const initialState = {
 };
 
 //Register User
-export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => { 
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
     try {
         return await authService.register(user)
     } catch (error) {
         const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
-        return thunkAPI.rejectWithValue(message)
+        return thunkAPI.rejectWithValue(message);
     }
 });
- 
+
+
 const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -33,10 +34,24 @@ const authSlice = createSlice({
             state.message = "";
         }
     },
-    extraReducers:() => {
+    extraReducers: (builder) => {
         //thunk function
+        builder
+            .addCase(register.pending, (state) => {
+            state.isLoading = true;
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true;
+                state.user = action.payload
+            })
+            .addCase(register.rejected, (state, action) => { 
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload
+            })
     },
 });
 
 export default authSlice.reducer;
-export const { reset } = authSlice.actions;
+export const { reset } = authSlice.actions; 
