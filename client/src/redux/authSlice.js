3283e87587dd9a58697bf2b5b166665ff4e4a1a6
Heytemisan <temyicy@gmail.com>
2022-08-05 12:24:58 +0100

    featL: called authService function to authSlice

--- client/src/redux/authSlice.js
+++ client/src/redux/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './appService';
 
 //Get user (data) from localStorage
 const user = JSON.parse(localStorage.getItem('user'));
@@ -11,9 +12,14 @@ const initialState = {
     message: ""
 };
 
-
 //Register User
-
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => { 
+    try {
+        return await authService.register(user)
+    } catch (error) {
+        
+    }
+})
 
 const authSlice = createSlice({
     name: "auth",
